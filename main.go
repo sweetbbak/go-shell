ec1442f86f23835fc37910e1cbc1087b685bbd84
meili <sweetbabyalaska@gmail.com>
2023-09-26 20:02:25 -0800

    idk what im doing

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"golang.org/x/term"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
 	"syscall"
 )
+
+type Termios syscall.Termios
 
 func System(cmd string) int {
 	c := exec.Command("sh", "-c", cmd)
@@ -52,20 +57,117 @@ func sys_exec() {
 	os.Exit(0)
 }
 
-func main() {
+func ClearScreen(w io.Writer) (int, error) {
+	return w.Write([]byte("\033[H"))
+}
 
-	reader := bufio.NewReader(os.Stdin)
+func SuspendMe() {
+	p, _ := os.FindProcess(os.Getppid())
+	p.Signal(syscall.SIGTSTP)
+	p, _ = os.FindProcess(os.Getpid())
+	p.Signal(syscall.SIGTSTP)
+}
+
+func get_stdin() int {
+	return syscall.Stdin
+}
+
+//	func init_env() {
+//		user := os.Getenv("USER")
+//		home := os.Getenv("HOME")
+//		term := os.Getenv("TERM")
+//	}
+//
+// Move cursor to given position
+func moveCursor(x int, y int) {
+	var screen *bytes.Buffer = new(bytes.Buffer)
+	fmt.Fprintf(screen, "\033[%d;%dH", x, y)
+}
+
+// Clear the terminal
+func clearTerminal() {
+	var output *bufio.Writer = bufio.NewWriter(os.Stdout)
+	output.WriteString("\033[2J")
+}
+
+func xterminal() error {
+	if !term.IsTerminal(0) || !term.IsTerminal(1) {
+		return fmt.Errorf("Stdin/Stdout should be a terminal")
+	}
+	oldState, err := term.MakeRaw(0)
+	if err != nil {
+		return err
+	}
+	defer term.Restore(0, oldState)
+	screen := struct {
+		io.Reader
+		io.Writer
+	}{os.Stdin, os.Stdout}
+
+	term := term.NewTerminal(screen, "")
+
+	home := os.Getenv("HOME")
+	cwd, _ := os.Getwd()
+	dir := strings.Replace(cwd, home, "~", 1)
+	term.SetPrompt(string(term.Escape.Red) + dir + " $ " + string(term.Escape.Reset))
+
+	// rePrefix := string(term.Escape.Cyan) + "Human says:" + string(term.Escape.Reset)
+
 	for {
-		dir, _ := os.Getwd()
-		fmt.Printf("[%s] $ ", dir)
-		cmdString, err := reader.ReadString('\n')
+		line, err := term.ReadLine()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if line == "" {
+			continue
+		}
+		err = runCommand(line)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
-		err = runCommand(cmdString)
+		// fmt.Fprintln(term, rePrefix, line)
+	}
+}
+
+func main() {
+	// if err := xterminal(); err != nil {
+	// 	fmt.Println("Error: ", err)
+	// }
+	// var screen *bytes.Buffer = new(bytes.Buffer)
+	// var output *bufio.Writer = bufio.NewWriter(os.Stdout)
+	// output.WriteString("\033[2J")
+	// fmt.Fprintf(screen, "%s", "")
+
+	home := os.Getenv("HOME")
+	reader := bufio.NewReader(os.Stdin)
+	var buf []byte
+
+	for {
+		cwd, _ := os.Getwd()
+		dir := strings.Replace(cwd, home, "~", 1)
+		fmt.Printf("\033[31m[%s] \033[0m ", dir)
+
+		cmd, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
+
+		// byte, err := reader.ReadByte()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		// buf = append(buf, byte)
+
+		if cmd != "\n" {
+			err = runCommand(cmd)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+		}
+		fmt.Println(string(buf))
 	}
 }
 
@@ -76,6 +178,9 @@ func runCommand(command string) error {
 	for x := 0; x < len(args); x++ {
 		if args[x] == "~" {
 			args[x] = os.Getenv("HOME")
+		}
+		if strings.Contains(args[x], "~") {
+			args[x] = strings.ReplaceAll(args[x], "~", os.Getenv("HOME"))
 		}
 	}
 
@@ -90,11 +195,17 @@ func runCommand(command string) error {
 		return os.Chdir(args[1])
 
 	case "ll":
-		ll := []string{"ls", "-l", "--color=always"}
+		ll := []string{"ls", "-lah", "--color=always"}
 		args = append(ll, args[1:]...)
+
+	case "ls":
+		ll := []string{"eza", "--icons", "--color=always"}
+		args = append(ll, args[1:]...)
+
 	}
 
 	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Env = os.Environ()
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	return cmd.Run()
